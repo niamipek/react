011a4b77be07ed82a22b4be7ec5f977d9abd8ba5
niamipek <binhquyen296@gmail.com>
2025-07-08 19:44:14 +0700

    phân trang

--- src/components/TableUsers.js
+++ src/components/TableUsers.js
@@ -1,18 +1,32 @@
 import Table from "react-bootstrap/Table";
 import React, { use, useEffect, useState } from "react";
-
+import ReactPaginate from 'react-paginate';
 import { fetchAllUsers } from "../services/UserService";
 const TableUsers = (props) => {
   const [listUsers, setListUsers] = useState([]);
+  const [totalUsers, setTotalUsers] = useState(0);
+  const [currentPage, setCurrentPage] = useState(0);
+  const usersPerPage = 5;
+  const offset = currentPage * usersPerPage;
+  const currentUsers = listUsers.slice(offset, offset + usersPerPage);
+
+  let totalUser = 0;
   useEffect(() => {
     getUsers();
   }, []);
   const getUsers = async () => {
     let res = await fetchAllUsers();
     if (res) {
+      console.log("check res", res.length);
+      totalUser=res.length;
+      console.log("totalUser", totalUser);
+      setTotalUsers(totalUser);
       setListUsers(res);
     }
   };
+  const handlePageClick = (event) => {
+   setCurrentPage(event.selected);
+  }
   console.log("listUsers", listUsers);
   // email: "Sincere@april.biz";
   // id: 1;
@@ -31,9 +45,9 @@ const TableUsers = (props) => {
           </tr>
         </thead>
         <tbody>
-          {listUsers &&
-            listUsers.length > 0 &&
-            listUsers.map((item, index) => {
+          {currentUsers &&
+            currentUsers.length > 0 &&
+            currentUsers.map((item, index) => {
               return (
                 <tr key={`users-${index}`}>
                   <td>{item.id}</td>
@@ -45,6 +59,25 @@ const TableUsers = (props) => {
             })}
         </tbody>
       </Table>
+      <ReactPaginate
+        breakLabel="..."
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={5}
+        pageCount={5}
+        previousLabel="< previous"
+        pageClassName="page-item"
+        pageLinkClassName="page-link"
+        previousClassName="page-item"
+        previousLinkClassName="page-link"
+        nextClassName="page-item"
+        nextLinkClassName="page-link"
+        breakClassName="page-item"
+        breakLinkClassName="page-link"
+        containerClassName="pagination"
+        activeClassName="active"
+
+      />
     </>
   );
 };

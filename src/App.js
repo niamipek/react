25f4342fa0d3a2d7dceca01783146b1db305f0b7
niamipek <binhquyen296@gmail.com>
2025-07-08 21:04:56 +0700

    add modal

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-
 import "./App.scss";
 import Header from "./components/Header";
 import TableUsers from "./components/TableUsers";
 import Container from "react-bootstrap/Container";
+import ModalAddNew from "./components/ModalAddNew";
+import React, { useState } from "react";
 function App() {
+  const [isShowModalAddNew, setShowModalAddNew] = useState(false);
+
   return (
     <div className="app-container">
       <Header />
       <Container>
-       
+        <div className="my-3 add-new" >
+          <span><b>List User:</b></span>
+          <button className="btn btn-primary" 
+          onClick={()=>setShowModalAddNew(true)}>Add new user</button>
+        </div>
         <TableUsers />
       </Container>
+    
+    <ModalAddNew
+    show={isShowModalAddNew}
+    handleClose={() => setShowModalAddNew(false)}/>
+    
     </div>
   );
 }
